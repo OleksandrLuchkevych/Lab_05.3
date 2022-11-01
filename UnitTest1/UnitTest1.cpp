55f7ae578d16b58e8bb93afb05991a2843a3ad17
OleksandrLuchkevych <114696247+OleksandrLuchkevych@users.noreply.github.com>
2022-11-02 00:04:29 +0200

    Add files via upload

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t;
+			double R;
+			t = (cos(0) + 1) / (exp(0) + sin(0) * sin(0));
+			R = -(1 / 4 * 0 + 2);
+			Assert::AreEqual(t, 2);
+			Assert::AreEqual(R, -0,5);
+		}
+	};
+}
